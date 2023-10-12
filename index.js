40de5e5d7f254bb9ebe3df0c391361b2cfe17639
Joyce Achieng <joyce.achieng@student.moringaschool.com>
2023-10-13 00:18:56 +0300

    add change event listener

--- index.js
+++ index.js
@@ -49,6 +49,43 @@ likeButtons.forEach((likeButton) => {
     .catch(error => {
       console.error("Error fetching cat:", error);
     });
-
+    const catBreedDropdown = document.getElementById("catBreed");
+    catBreedDropdown.addEventListener("change", (e) => {
+      const selectedBreed = e.target.value;
+      // Call a function to update the displayed content based on the selected breed
+      updateContentForBreed(selectedBreed);
+    });
+    function updateContentForBreed(selectedBreed) {
+      // Use a switch statement or an object to define content for each breed
+      const breedContent = {
+        "persian": {
+          title: "Persian",
+          description: "Big round eyes inside a round face give Persians their unmistakable look. Add in the flat face and the distinctive profile, or smushed nose, and you know you’ve found a gorgeous, soft, sweet-tempered Persian cat. Affectionate toward their owners, children, and other pets, Persians are generally not very active, preferring a quiet environment. Averaging between 3-6 kg (7-12 lbs) as adults, they are on the smaller side but will provide lots of cuddling and companionship during rest periods. Because of their semi-dense coats, they overheat easily and can require professional grooming to prevent matted fur.",
+          
+        },
+        "maine-coon": {
+          title: "Maine Coon",
+          description: "The gentle giants of the cat world, Maine Coons are magnificent when they reach their tallest height of 16 inches, length of up to 100 cm (40 in) and weigh in at 3.6-8.2 kg (7-18 lbs) or more. They have lovely tufts of fur in their ears and under their paws, a square face and a big fluffy tail. They are friendly, inquisitive, and very hardy, meaning that they can live in a variety of climates and require less care and attention than other breeds. That’s not to say a Maine Coon won’t want attention. They love to be admired as much as the next fur baby.  They are curious and fascinated by water.",
+          // Add more content for Maine Coon breed as needed
+        },
+        // Add content for other breeds as needed
+      };
+    
+      // Get the breed information based on the selected breed
+      const selectedBreedInfo = breedContent[selectedBreed];
+    
+      // Update the content based on the selected breed
+      const breedInfoContainer = document.getElementById("breedInfo");
+      if (selectedBreedInfo) {
+        breedInfoContainer.innerHTML = `
+          <h2>${selectedBreedInfo.title}</h2>
+          <p>${selectedBreedInfo.description}</p>
+          <!-- Add more content as needed -->
+        `;
+      } else {
+        breedInfoContainer.innerHTML = "Breed information not found.";
+      }
+    }
+     
   })
 
