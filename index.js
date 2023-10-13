6bb35c6ddc4740667a51a9b779395cb4ae214543
Joyce Achieng <joyce.achieng@student.moringaschool.com>
2023-10-13 20:56:10 +0300

    adding breed and its images

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ let isLiked = false;
     });
     const catBreedDropdown = document.getElementById("catBreed");
     catBreedDropdown.addEventListener("change", (e) => {
-      const selectedBreed = e.target.value;
+     const selectedBreed = e.target.value;
       updateContentForBreed(selectedBreed);
     });
     function updateContentForBreed(selectedBreed) {
@@ -76,16 +76,22 @@ let isLiked = false;
         "persian": {
           title: "Persian",
           description: "Big round eyes inside a round face give Persians their unmistakable look. Add in the flat face and the distinctive profile, or smushed nose, and you know you’ve found a gorgeous, soft, sweet-tempered Persian cat. Affectionate toward their owners, children, and other pets, Persians are generally not very active, preferring a quiet environment. Averaging between 3-6 kg (7-12 lbs) as adults, they are on the smaller side but will provide lots of cuddling and companionship during rest periods. Because of their semi-dense coats, they overheat easily and can require professional grooming to prevent matted fur.",
-          
+          Image: "https://www.cathealthy.ca/wp-content/uploads/2021/11/Persian-1024x1024-1.jpg"
         },
         "maine-coon": {
           title: "Maine Coon",
           description: "The gentle giants of the cat world, Maine Coons are magnificent when they reach their tallest height of 16 inches, length of up to 100 cm (40 in) and weigh in at 3.6-8.2 kg (7-18 lbs) or more. They have lovely tufts of fur in their ears and under their paws, a square face and a big fluffy tail. They are friendly, inquisitive, and very hardy, meaning that they can live in a variety of climates and require less care and attention than other breeds. That’s not to say a Maine Coon won’t want attention. They love to be admired as much as the next fur baby.  They are curious and fascinated by water.",
-          
+          Image : "https://www.cathealthy.ca/wp-content/uploads/2021/11/Maine_Coon-1024x1024-1.jpg"
         },
        
-      };
-    
+      
+      "scottish-fold": {
+        title: "Scottish Fold",
+        description: "You don’t have to be Sherlock Holmes to know that the Scottish Fold cat is named for its distinctive folded ears and hails from Scotland. Their hearing is just as acute as any cat and their ears can change directions, lay back flat, and stand straight up. As adults, they are compact and weigh about 3.5-5.5 kg (8-12 lbs). Gentle, mild mannered, and intelligent, Scottish Fold cats adapt well to new owners and residences. They are good family cats but they tend to single out one favourite person among their owners. They will require more grooming because of their dense coats and may retain more ear wax but generally aren’t more prone to ear mites or infections than their prick-eared cousins. ",
+         Image :"https://www.cathealthy.ca/wp-content/uploads/2021/11/Scottish_Fold-1024x1024-1.jpg"
+      
+        },
+    };
       
       const selectedBreedInfo = breedContent[selectedBreed];
     
@@ -95,6 +101,7 @@ let isLiked = false;
         breedInfoContainer.innerHTML = `
           <h2>${selectedBreedInfo.title}</h2>
           <p>${selectedBreedInfo.description}</p>
+          <img src="${selectedBreedInfo.Image}" alt="${selectedBreedInfo.title}" />
           
         `;
       } else {
